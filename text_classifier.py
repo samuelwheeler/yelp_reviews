fd3fa40c90b56edfab0beaca1642faefda0af87f
samuelwheeler <91495954+samuelwheeler@users.noreply.github.com>
2023-04-10 11:55:44 -0500

    Update text_classifier.py

--- text_classifier.py
+++ text_classifier.py
@@ -58,16 +58,55 @@ class Attention(nn.Module):
         out = torch.matmul(attn, v)
         out = rearrange(out, 'b h n d -> b n (h d)')
         return self.to_out(out)
+    
+
+class NoSoftMaxAttention(nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+        super().__init__()
+        inner_dim = dim_head *  heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        #self.scale = dim_head ** -0.5
+
+        #self.attend = nn.Softmax(dim = -1)
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, x):
+        qkv = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), qkv)
+
+        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
+
+        #attn = self.attend(dots)
+
+        out = torch.matmul(dots, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+
 
 class Transformer(nn.Module):
-    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, attention_type, dropout = 0.):
         super().__init__()
         self.layers = nn.ModuleList([])
-        for _ in range(depth):
-            self.layers.append(nn.ModuleList([
-                PreNorm(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
-                PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
-            ]))
+        if attention_type == 'standard':
+            for _ in range(depth):
+                self.layers.append(nn.ModuleList([
+                    PreNorm(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
+                    PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
+                ]))
+        elif attention_type == 'no_softmax':
+            for _ in range(depth):
+                self.layers.append(nn.ModuleList([
+                    PreNorm(dim, NoSoftMaxAttention(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
+                    PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
+                ]))
+
     def forward(self, x):
         for attn, ff in self.layers:
             x = attn(x) + x
@@ -86,7 +125,7 @@ class EmbeddingLayer(nn.Module):
         return self.embedding_layer(x)
 
 class Review_Classifier(nn.Module):
-    def __init__(self, *, embedding_weights, sentence_length, embedding_dim, num_classes, dim, depth, heads, mlp_dim, pool = 'cls',  dim_head = 64, dropout = 0., emb_dropout = 0.):
+    def __init__(self, *, embedding_weights, sentence_length, embedding_dim, num_classes, dim, depth, heads, mlp_dim, attention_type = 'standard',  pool = 'cls',  dim_head = 64, dropout = 0., emb_dropout = 0.):
         super().__init__()
         
 
@@ -94,11 +133,11 @@ class Review_Classifier(nn.Module):
         assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
 
         
-        self.pos_embedding = nn.Parameter(torch.randn(1, 101, embedding_dim))
+        self.pos_embedding = nn.Parameter(torch.randn(1, sentence_length + 1, embedding_dim))
         self.cls_token = nn.Parameter(torch.randn(1, 1, embedding_dim))
         self.dropout = nn.Dropout(emb_dropout)
 
-        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
+        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout, attention_type = attention_type)
 
         self.pool = pool
         #self.to_latent = nn.Identity()
