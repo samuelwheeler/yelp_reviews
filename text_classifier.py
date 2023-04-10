f7c1a9c5bcce93747ce38da998f6ce88081c8ef7
samuelwheeler <91495954+samuelwheeler@users.noreply.github.com>
2023-04-10 12:07:24 -0500

    Update text_classifier.py

--- text_classifier.py
+++ text_classifier.py
@@ -88,6 +88,38 @@ class NoSoftMaxAttention(nn.Module):
         out = torch.matmul(dots, v)
         out = rearrange(out, 'b h n d -> b n (h d)')
         return self.to_out(out)
+    
+
+class QuinticAttetion(nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+        super().__init__()
+        inner_dim = dim_head *  heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        #self.scale = dim_head ** -0.5
+
+        #self.attend = nn.Softmax(dim = -1)
+        self.to_qkv = nn.Linear(dim, inner_dim * 5, bias = False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, x):
+        qkv = self.to_qkv(x).chunk(5, dim = -1)
+        q1, k1, q2, k2, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), qkv)
+
+        dots1 = torch.matmul(q1, k1.transpose(-1, -2))
+        dots2 = torch.matmul(q2, k2.transpose(-1, -2))
+        dots = dots1@dots2
+        #attn = self.attend(dots)
+
+        out = torch.matmul(dots, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+
 
 
 class Transformer(nn.Module):
@@ -104,6 +136,12 @@ class Transformer(nn.Module):
             for _ in range(depth):
                 self.layers.append(nn.ModuleList([
                     PreNorm(dim, NoSoftMaxAttention(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
+                    PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
+                ]))
+        elif attention_type == 'quintic':
+            for _ in range(depth):
+                self.layers.append(nn.ModuleList([
+                    PreNorm(dim, QuinticAttetion(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
                     PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
                 ]))
 
