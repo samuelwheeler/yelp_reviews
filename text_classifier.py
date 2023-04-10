8d225b84bdb6c5d30770fd8ef031d168a2ad2ca4
samuelwheeler <91495954+samuelwheeler@users.noreply.github.com>
2023-04-10 11:59:52 -0500

    Update text_classifier.py

--- text_classifier.py
+++ text_classifier.py
@@ -51,7 +51,7 @@ class Attention(nn.Module):
         qkv = self.to_qkv(x).chunk(3, dim = -1)
         q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), qkv)
 
-        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
+        dots = torch.matmul(q, k.transpose(-1, -2))
 
         attn = self.attend(dots)
 
